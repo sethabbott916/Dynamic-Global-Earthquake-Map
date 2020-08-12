4c7ae19032397c577cebacdee4aeedc2304773e4
SethAbbott916 <sethcode91@gmail.com>
2020-08-12 15:51:07 -0700

    added dark layer

--- leaflet-step1/static/js/logic.js
+++ leaflet-step1/static/js/logic.js
@@ -27,19 +27,14 @@ var dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{
   accessToken: API_KEY
 });
 
-var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "satellite-v9",
-  accessToken: API_KEY
-});
+
 
 // Create two separate layer groups: one for cities and one for states
 
 var earthquakelayer = L.layerGroup();
 var faultlayer = L.layerGroup();
 
-var basemaps = {"base": basemap, "light": light, "dark": dark, "satellite": satellite}
+var basemaps = {"base": basemap, "light": light, "dark": dark}
 var overlayMaps = { "Earthquakes": earthquakelayer, "Fault Lines": faultlayer }
 
 // Pass our map layers into our layer control
