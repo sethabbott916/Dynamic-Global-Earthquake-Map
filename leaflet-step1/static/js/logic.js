908fb500069916946ca7b148a10260e8d89b6e90
SethAbbott916 <sethcode91@gmail.com>
2020-08-11 23:21:52 -0700

    create markers function

--- leaflet-step1/static/js/logic.js
+++ leaflet-step1/static/js/logic.js
@@ -7,3 +7,20 @@ d3.json(queryUrl, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
   createMarkers(data.features);
 });
+
+
+
+function createMarkers (earthquakeData) {
+    // Define a function we want to run once for each feature in the features array
+    // Give each feature a popup describing the place and time of the earthquake
+    function createMarker(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.place +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    }
+
+
+
+}
+
+
+
