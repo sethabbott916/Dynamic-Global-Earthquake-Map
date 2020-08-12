2d9b11bf97d97554990cae21e4ff8af10319e791
SethAbbott916 <sethcode91@gmail.com>
2020-08-11 23:19:44 -0700

    load url json

--- leaflet-step1/static/js/logic.js
+++ leaflet-step1/static/js/logic.js
@@ -1,15 +1,9 @@
-var myMap = L.map("map", {
-    center: [45.52, -122.67],
-    zoom: 2
-  });
-  
-  // Adding a tile layer (the background map image) to our map
-  // We use the addTo method to add objects to our map
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  }).addTo(myMap);
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+  console.log(data.features)
+  // Once we get a response, send the data.features object to the createFeatures function
+  createMarkers(data.features);
+});
